7d33031424964e55d1cd4b7729a1d5c5c0a13597
Peter Stone <peter.stone.online@gmail.com>
2018-09-24 16:49:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if (speed > 39 && speed < 61)
+    false 
+  else
+    true 
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed > 39 && speed < 61) ? false : true 
 end
 	
 
